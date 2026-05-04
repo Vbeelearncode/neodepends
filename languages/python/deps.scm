68cff668c93c0b6aab4af1b9944ea632e134ec64
Bao Vuong <baovg.a1.k2023@gmail.com>
2026-05-03 20:33:53 -0400

    feat: added return type inference query on Tree-sitter result

--- languages/python/deps.scm
+++ languages/python/deps.scm
@@ -21,6 +21,14 @@
     object: (identifier) @call.class_recv
     attribute: (identifier) @call.class_method))
 
+(call
+  function: (attribute
+    object: (attribute
+      object: (identifier) @_self_field_self
+      attribute: (identifier) @call.self_field_recv)
+    attribute: (identifier) @call.self_field_method)
+  (#eq? @_self_field_self "self"))
+
 (class_definition
   (argument_list
     (identifier) @extend.base))
@@ -40,12 +48,59 @@
     (identifier) @param.name
     type: (type (identifier) @param.type)))
 
+(function_definition
+  parameters: (parameters
+    (identifier) @function.param))
+
+(function_definition
+  parameters: (parameters
+    (typed_parameter
+      (identifier) @function.param)))
+
+(expression_statement
+  (assignment
+    left: (attribute
+      object: (identifier) @_field_assign_self
+      attribute: (identifier) @field_assign.field)
+    right: (identifier) @field_assign.param)
+  (#eq? @_field_assign_self "self"))
+
 (expression_statement
   (assignment
     left: (identifier) @assign.var
     right: (call
       function: (identifier) @assign.class
       arguments: (argument_list))))
+
+(expression_statement
+  (assignment
+    left: (identifier) @return_assign.var
+    right: (call
+      function: (identifier) @return_assign.call
+      arguments: (argument_list))))
+
+(expression_statement
+  (assignment
+    left: (identifier) @return_assign_attr.var
+    right: (call
+      function: (attribute
+        object: (identifier) @return_assign_attr.recv
+        attribute: (identifier) @return_assign_attr.method)
+      arguments: (argument_list))))
+
+(return_statement
+  (identifier) @return.var)
+
+(return_statement
+  (attribute
+    object: (identifier) @_return_self
+    attribute: (identifier) @return.self_field)
+  (#eq? @_return_self "self"))
+
+(return_statement
+  (call
+    function: (identifier) @return.call
+    arguments: (argument_list)))
 
 (decorated_definition
   (decorator
